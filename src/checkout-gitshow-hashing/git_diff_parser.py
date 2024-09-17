a94bddc930933e9c26ea77b603a2036e009d3f7c
Irtaza Sajid Qureshi <stwaynexg@gmail.com>
2024-09-17 14:33:49 -0700

    fix issue for error with utf-8 decoding

--- src/checkout-gitshow-hashing/git_diff_parser.py
+++ src/checkout-gitshow-hashing/git_diff_parser.py
@@ -4,5 +4,5 @@ class GitDiffParser:
     @staticmethod
     def get_diff(project_dir):
         cmd = "git show --no-prefix -U500"
-        result = subprocess.run(cmd, cwd=project_dir, shell=True, capture_output=True, text=True, check=True)
+        result = subprocess.run(cmd, cwd=project_dir, shell=True, capture_output=True, text=True, check=True, errors='ignore')
         return result.stdout
