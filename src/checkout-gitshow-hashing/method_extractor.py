3cad550c86b3e790467f139c0d87eba8b1afadd1
Irtaza Sajid <stwaynexg@gmail.com>
2025-05-20 20:33:39 -0400

    Update method_extractor.py

--- src/checkout-gitshow-hashing/method_extractor.py
+++ src/checkout-gitshow-hashing/method_extractor.py
@@ -96,7 +96,7 @@ class MethodExtractor:
                 method_line_number = MethodExtractor._find_method_by_line(method_line, content)
                 if method_line_number:
                     print(f"Method position for {method_line}: {method_line_number}")
-                    method_body = MethodExtractor._find_method_body(method_line_number, content)
+                    method_body = MethodExtractor._find_method_body((method_line_number, 0), content)
                     method_implementations[method_line] = method_body
 
         return method_implementations
