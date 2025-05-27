a70f373dd997cec0bdff78767fc511658af01203
Irtaza Sajid <stwaynexg@gmail.com>
2025-05-27 16:54:52 -0400

    Update run_d4j.sh

--- src/checkout-gitshow-hashing/run_d4j.sh
+++ src/checkout-gitshow-hashing/run_d4j.sh
@@ -6,6 +6,6 @@ REPO_DIR="/tmp/repos/"
 for proj in $(defects4j pids); do
     for bug in $(cut -f1 -d',' "$D4J_HOME/framework/projects/$proj/commit-db"); do
         python3.9 src/checkout-gitshow-hashing/main.py --project $proj --bug_num $bug
-        rm -rf $REPO_DIR/${proj}_${bug}
+        # rm -rf $REPO_DIR/${proj}_${bug}
     done
 done
