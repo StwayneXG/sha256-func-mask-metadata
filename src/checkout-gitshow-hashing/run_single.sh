0f205eea0664bbbcea34c6f2dd0de29858fb3240
Irtaza Sajid <stwaynexg@gmail.com>
2025-06-02 19:19:17 -0400

    debugging remaining cases

--- src/checkout-gitshow-hashing/run_single.sh
+++ src/checkout-gitshow-hashing/run_single.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PROJECT="Math"
-BUG_NUM=71
+PROJECT="Mockito"
+BUG_NUM=32
 REPO_DIR="/tmp/repos/"
 
 python3.9 src/checkout-gitshow-hashing/main.py --project $PROJECT --bug_num $BUG_NUM
