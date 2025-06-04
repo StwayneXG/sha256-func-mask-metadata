00500f4d7d946a4989dac2052097b673683971f6
Irtaza Sajid <stwaynexg@gmail.com>
2025-06-04 19:31:24 -0400

    Update run_single.sh

--- src/checkout-gitshow-hashing/run_single.sh
+++ src/checkout-gitshow-hashing/run_single.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PROJECT="Jsoup"
-BUG_NUM=25
+PROJECT="Chart"
+BUG_NUM=1
 REPO_DIR="/tmp/repos/"
 
 python3.9 src/checkout-gitshow-hashing/main.py --project $PROJECT --bug_num $BUG_NUM
