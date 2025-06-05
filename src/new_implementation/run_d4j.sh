9054bfa0078807c63460335e1bf571e1d57f1ead
Irtaza Sajid <stwaynexg@gmail.com>
2025-06-04 22:48:46 -0400

    Update run_d4j.sh

--- src/new_implementation/run_d4j.sh
+++ src/new_implementation/run_d4j.sh
@@ -8,7 +8,5 @@ for proj in $(defects4j pids); do
         python3.9 src/new_implementation/driver.py --diff-file /root/data/Defects4J/project_diffs/${proj}_${bug}.diff --repo-root /tmp/repos/${proj}_${bug}/ --out-csv /root/data/Defects4J/relevent_gt_data/${proj}_${bug}.csv
         # python3.9 src/checkout-gitshow-hashing/main.py --project $proj --bug_num $bug
         # rm -rf $REPO_DIR/${proj}_${bug}
-        break
     done
-    break
 done
