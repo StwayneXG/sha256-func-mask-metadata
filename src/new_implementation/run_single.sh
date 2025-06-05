a5c8176f3fd762e64e6c828be92b70e1e519206b
Irtaza Sajid <stwaynexg@gmail.com>
2025-06-05 05:16:22 -0400

    Update run_single.sh

--- src/new_implementation/run_single.sh
+++ src/new_implementation/run_single.sh
@@ -1,3 +1,3 @@
-proj="Codec"
-bug=13
+proj="Lang"
+bug=56
 python3.9 src/new_implementation/driver.py --diff-file /root/data/Defects4J/project_diffs/${proj}_${bug}.diff --repo-root /tmp/repos/${proj}_${bug}/ --out-csv /root/data/Defects4J/relevent_gt_data/${proj}_${bug}.csv
